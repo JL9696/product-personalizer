28453168d6da08b577e4e38b0dab7a2bc9e03e8d
Joanna Lew/javascript-blog.git <lewjoanna96@gmail.com>
2023-03-29 19:06:40 +0200

    add getPrice and cartSummary function

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,37 +1,72 @@
 import styles from './Product.module.scss';
 import clsx from 'clsx';
 import Button from '../Button/Button';
+import PropTypes from 'prop-types';
+import { useState } from 'react';
+import shortid from 'shortid';
 
-const Product = props => {
+const Product = ({ id, name, title, basePrice, colors, sizes }) => {
+
+  const [currentColor, setCurrentColor] = useState(colors[0]);
+  const [currentSize, setCurrentSize] = useState(sizes[0].name);
+  const [currentPrice, setCurrentPrice] = useState(sizes[0].additionalPrice);
+  const prepareColorClassName = color => {
+    return styles['color' + color[0].toUpperCase() + color.substr(1).toLowerCase()];
+  };
+  const getPrice = (basePrice, currentPrice) => {
+    return basePrice + currentPrice;
+  };
+
+  const cartSummary = e => {
+    e.preventDefault();
+    console.log('udało się');
+    console.log('Summary');
+    console.log('==============');
+    console.log('Name: ', title);
+    console.log('Price: ', getPrice(basePrice, currentPrice));
+    console.log('Size: ', currentSize);
+    console.log('Color: ', currentColor);
+  };
+
   return (
     <article className={styles.product}>
       <div className={styles.imageContainer}>
-        <img 
-          className={styles.image}
-          alt="Kodilla shirt"
-          src={`${process.env.PUBLIC_URL}/images/products/shirt-kodilla--black.jpg`} />
+        <img className={styles.image} alt={title} src={`${process.env.PUBLIC_URL}/images/products/shirt-${name}--${currentColor}.jpg`} />
       </div>
+
       <div>
         <header>
-          <h2 className={styles.name}>Kodilla shirt</h2>
-          <span className={styles.price}>Price: 20$</span>
+          <h2 className={styles.name}>{title}</h2>
+          <span className={styles.price}>Price: {getPrice(basePrice, currentPrice)}$</span>
         </header>
-        <form>
+
+        <form onSubmit={cartSummary}>
           <div className={styles.sizes}>
             <h3 className={styles.optionLabel}>Sizes</h3>
             <ul className={styles.choices}>
-              <li><button type="button" className={styles.active}>S</button></li>
-              <li><button type="button">M</button></li>
-              <li><button type="button">L</button></li>
-              <li><button type="button">XL</button></li>
+              {sizes.map((size) => (
+                <li key={shortid()}>
+                  <button type='button' onClick={() => {
+                    setCurrentSize(size.name);
+                    setCurrentPrice(size.additionalPrice);
+                    }} 
+                    className={clsx(size.name === currentSize && styles.active)}>
+                    {size.name}
+                  </button>
+                </li>))}
             </ul>
           </div>
+
           <div className={styles.colors}>
             <h3 className={styles.optionLabel}>Colors</h3>
             <ul className={styles.choices}>
-              <li><button type="button" className={clsx(styles.colorBlack, styles.active)} /></li>
-              <li><button type="button" className={clsx(styles.colorRed)} /></li>
-              <li><button type="button" className={clsx(styles.colorWhite)} /></li>
+              {colors.map((color) => (
+                <li key={shortid()}>
+                <button type='button' onClick={() => { setCurrentColor(color)
+                }}
+                className={clsx(prepareColorClassName(color), color === currentColor && styles.active)}/>
+              </li>
+              ))}
             </ul>
           </div>
           <Button className={styles.button}>
@@ -41,6 +76,15 @@ const Product = props => {
       </div>
     </article>
   )
+};
+
+Product.propTypes = {
+  id: PropTypes.number.isRequired,
+  name: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  basePrice: PropTypes.number.isRequired,
+  colors: PropTypes.array.isRequired,
+  sizes: PropTypes.array.isRequired,
 };
 
 export default Product;
